85885a0fe99be6159565c4bc4f7c389264c1bea6
marienailah218-del <marienailah218@gmail.com>
2026-02-11 09:23:51 +0300

    first commit

---
+++ python-basics/challenge1.py
@@ -0,0 +1,9 @@
+university="JKUAT"
+grade= "C"         
+course= "computer science"
+cluster_points= "69"
+
+print(f"my favourite university is {university}")
+print(f"my KCSE grade is {grade}")
+print(f"my favourite course is {course}")
+print(f"my cluster points are {cluster_points}")
---
+++ python-basics/hello.py
@@ -0,0 +1,14 @@
+# This is a comment
+print("Hello from Nailah")
+# city/town
+print("i am from kiambu")
+# my hobbies
+print("i like to draw,sing and watch youtube videos")
+
+age=18
+height=1.00
+weight=50
+
+print(f"i am {age} years old")
+print(f"i am also {height} metres")
+print(f"and weigh {weight} kgs")
