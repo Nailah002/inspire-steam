872dc14f3d94467cdd879ae79dc6c5fcea2dca8d
marienailah218-del <marienailah218@gmail.com>
2026-02-12 23:55:05 +0300

    string_format,py

---
+++ python-basics/string_formating.py
@@ -0,0 +1,44 @@
+#Name: Nailah Wanjiku
+#Date: 12/2/2026
+#string_formating.py
+
+#capitalize
+sentence = "confirmed:"
+capitalized = sentence.upper()
+
+print(" ", capitalized)
+
+#splitting string
+sentence = "You have received 50.0kes from Phillip"
+split = sentence.split("  ")
+
+print(f" ", split[0])
+
+
+
+
+
+
+
+#junior developers
+#"CONFIRMED : You have received 50kes from Phillip "
+
+balance = "70.02kes"
+amount_added = "50kes"
+
+cleaned_balance = float (balance.replace("kes", ""))
+cleaned_amount_added =float (amount_added.replace("kes", ""))
+
+print("cleaned balance: ", cleaned_amount_added)
+
+#previous balance
+previous_balance = "balance"
+
+print("Previous balance: ", balance)
+
+#finding new balance
+new_balance = (cleaned_balance) + (cleaned_amount_added)
+
+print("Your new balance is: ", new_balance)
+
+
--- python-basics/user_input.py
+++ python-basics/user_input.py
@@ -7,10 +7,10 @@ first_number=20
 second_number=90
 
 
-first_number+second_number == "sum_number"
-first_number-second_number == "difference_number"
-first_number*second_number == "product_number"
-first_number/second_number == "quotient_number"
+first_number+second_number == "sum_numbers"
+first_number-second_number == "difference_numbers"
+first_number*second_number == "product_numbers"
+first_number/second_number == "quotient_numbers"
 
 
 print(int(f"sum_number"))
